5c30fffef2da9e3bc22435bf7bfbd6e1afbf5067
Jama, Najib A <najama@student.rtc.edu>
2019-08-03 02:03:05 -0700

    Added Genre Class and implementation to movie class

--- GenericsClassLibrary/Movies.cs
+++ GenericsClassLibrary/Movies.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace GenericsClassLibrary
 {
-   public class Movies
+    public class Movies
     {
         public string Title { get; set; }
         public int ReleaseYear { get; set; }
-        
+        public Genre Genre { get; set; }
+
+        public Movies(string NameOfMovie, int Year, Genre MovieGenre)
+        {
+            Title = NameOfMovie;
+            ReleaseYear = Year;
+            Genre = MovieGenre;
+        }
+
+        public string GetMovieDetails()
+        {
+            return $"Title: {Title}  Year: {ReleaseYear}  Genre: {Genre.Name}";
+        }
 
     }
 }
