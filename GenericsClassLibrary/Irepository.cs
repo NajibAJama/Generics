23ecabe80ee9db533ef81a09c6bf79523c218f79
Jama, Najib A <najama@student.rtc.edu>
2019-08-03 02:42:23 -0700

    Added IRepository Interface

---
+++ GenericsClassLibrary/Irepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsClassLibrary
+{
+    public interface IRepository<T> where T:  class
+    {
+        void Add(T Obj);
+        T Rmeove(T obj);
+        T F(T obj);
+        IEnumerable<T> GetAll();
+        
+    }
+}
