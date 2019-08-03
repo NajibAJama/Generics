dd59d3782ee53381cef7bbf837546c0166f2739b
Jama, Najib A <najama@student.rtc.edu>
2019-08-03 01:52:30 -0700

    Movies Class added

---
+++ GenericsClassLibrary/Movies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsClassLibrary
+{
+   public class Movies
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        
+
+    }
+}
